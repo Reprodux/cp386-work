16a1075b4a8e80008d68d61e2f0651be1b3c638b
Brian <ha.briann@gmail.com>
2022-01-20 22:41:27 -0500

    updated files for a01

--- a01/z_creator.c
+++ a01/z_creator.c
@@ -1,19 +1,23 @@
 #include <stdlib.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <stdio.h>
+
 int main()
 {
-    // Fork returns process id
-    // in parent process
     pid_t child_pid = fork();
-  
-    // Parent process 
-    if (child_pid > 0)
-        sleep(50);
-  
-    // Child process
-    else        
+
+    if(child_pid > 0){
+        printf("In Parent Process ! \n"); //This makes the parent process sleep
+        system("ps -l");
+        printf("\n");
+        
+        sleep(10); 
+    } else {
+        printf("In child process rn ! \n"); 
         exit(0);
-  
+        
+    }
+    
     return 0;
 }
